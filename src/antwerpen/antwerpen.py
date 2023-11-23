79ee981cd5a35fe0966aa9a5f43717e3b0e85d7e
Klaas Schoute <klaas_schoute@hotmail.com>
2023-11-23 12:35:04 +0100

    Fix import and update async timeout (#338)

--- src/antwerpen/antwerpen.py
+++ src/antwerpen/antwerpen.py
@@ -5,18 +5,14 @@ import asyncio
 import socket
 from dataclasses import dataclass
 from importlib import metadata
-from typing import TYPE_CHECKING, Any
+from typing import Any, Self
 
-import async_timeout
 from aiohttp import ClientError, ClientSession
 from aiohttp.hdrs import METH_GET
 from yarl import URL
 
 from .exceptions import ODPAntwerpenConnectionError, ODPAntwerpenError
 from .models import DisabledParking
-
-if TYPE_CHECKING:
-    from typing_extensions import Self
 
 
 @dataclass
@@ -71,7 +67,7 @@ class ODPAntwerpen:
             self._close_session = True
 
         try:
-            async with async_timeout.timeout(self.request_timeout):
+            async with asyncio.timeout(self.request_timeout):
                 response = await self.session.request(
                     method,
                     url,
